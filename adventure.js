34588276de8b8b31c5e4cbaa0e3648eb473612bd
Ronak Singh <rssobti@outlook.com>
2017-03-07 14:38:03 -0600

    adventure

---
+++ adventure.js
@@ -0,0 +1,115 @@
+var person{
+  location:null,
+  items:[sword,shield,necklace],
+  health: 100,
+  health: function (a){
+    console.log(health);
+  }
+
+}
+
+person.health(20)
+
+
+if (Cave) {
+
+
+}
+
+
+switch (location) {
+  case 'mountains':
+    prompt('Mountains are great, do you want to hang glide or hike?')
+    break;
+
+  case 'caves':
+    console.log('precious')
+    break;
+
+  case 'sea':
+    console.log('Yaaar')
+    break;
+
+  default:
+    console.log('You cant go there yet.');
+}]
+
+
+
+
+if (location='sea') {
+  console.log('sea works');
+
+  }else if (location='caves') {
+    console.log('caves work');
+  }
+  else if (location='mountains') {
+    console.log('mountains');
+  }
+
+
+
+if (location='mountains') {
+  console.log('mountains works');
+
+}
+
+
+if (location='caves') {
+  console.log('cave works');
+
+}
+
+
+var name = prompt('What is you name?');
+
+var location = prompt('Would you like to go to the Mountains, Caves, or Sea?');
+
+
+if (location='sea') {
+  console.log('sea works');
+}
+//   else if (location='caves') {
+//     console.log('caves work');
+//   }
+//   else if (location='mountains') {
+//     console.log('mountains');
+//   }
+
+// }
+
+
+if (location='sea') {
+  console.log('sea works');
+
+  }else if (location='caves') {
+    console.log('caves work');
+  }
+  else if (location='mountains') {
+    console.log('mountains');
+  }
+
+
+
+
+
+
+// switch (location) {
+//   case 'mountains':
+//     console.log('Yodel');
+//     break;
+
+//   case 'caves':
+//     console.log('precious');
+//     break;
+
+//   case 'sea':
+//     console.log('Yaaar');
+//     break;
+
+//   default:
+//     console.log('You cant go there yet.');
+// }
+
+
+I tried yo, my coding life is in shambles.
